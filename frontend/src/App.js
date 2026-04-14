12192e2fc651e9571e28772270d51487ed63ce93
sowrav72 <sowravm72@gmail.com>
2026-04-14 18:07:56 +0600

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef, useCallback, useMemo, memo } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
+import * as Icons from "./icons";
 import "./App.css";
 import Navbar from "./Navbar";
 import Login from "./Login";
