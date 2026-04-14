b1f4422e1da761046fc5e22ff714530b40bb67d0
sowrav72 <sowravm72@gmail.com>
2026-04-14 17:10:16 +0600

    msg

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link, useNavigate, useLocation } from "react-router-dom";
+import { AirplaneIcon, HomeIcon } from "./icons";
 
 export default function Navbar() {
   const [scrolled, setScrolled] = useState(false);
